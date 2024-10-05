be6ce81e7eea2352b93442ab8794c02729c9bbd7
saulin18 <sondonsaul@gmail.com>
2024-10-04 23:30:46 -0400

    fixed auth urls

--- quizhub/urls.py
+++ quizhub/urls.py
@@ -21,5 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('quizes/', include('quizhub.quizes.urls')),
-    path('auth/', include('quizhub.users.urls')),
+    path('', include('quizhub.users.urls')),
 ]
