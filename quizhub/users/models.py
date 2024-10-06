f8659c71819549fa5efda09b2a3ed677c5026a43
saulin18 <sondonsaul@gmail.com>
2024-10-06 12:08:36 -0400

    changes in the register and user model

--- quizhub/users/models.py
+++ quizhub/users/models.py
@@ -1,20 +1,49 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
-
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import Permission
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
 # Create your models here.
 
-class RegisterUser(AbstractBaseUser):
-    username = models.CharField(max_length=100, unique=True)
-    password = models.CharField(max_length=100)
-    is_admin = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError('The given username must be set')
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+class RegisterUser(AbstractUser):
+    objects = CustomUserManager()
     USERNAME_FIELD = 'username'
     
+    groups = models.ManyToManyField(
+        Group,
+        related_name='custom_user_set',  # Change this to something unique
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions '
+                  'granted to each of their groups.',
+        verbose_name='groups',
+    )
     
-   
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='custom_user_set',  # Change this to something unique
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
     
     class Meta: 
         app_label = 'users'
+  
+
+
+
+  
     
    
     
--- quizhub/users/views.py
+++ quizhub/users/views.py
@@ -6,17 +6,26 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from quizhub.users.models import RegisterUser
 from .serializers import MyTokenObtainPairSerializer, RegisterUserSerializer
-
+from .models import RegisterUser as CustomUser
 # Create your views here.
 
 @api_view(['POST'])
 def register(request):
-    user = RegisterUser.objects.create_user(username=request.data['username'], password=request.data['password'])
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user = CustomUser.objects.create_user(username=username, password=password)
     serializer = RegisterUserSerializer(user, many=False)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def get_user(request):
+    username = request.query_params.get('username')
+    user = RegisterUser.objects.get(username=username)
+    serializer = RegisterUserSerializer(user)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 class get_users(generics.ListAPIView):
     serializer_class = RegisterUserSerializer
