7c712158689e9450e603a790330c2867c6fd034b
saulin18 <sondonsaul@gmail.com>
2024-10-07 18:04:32 -0400

    updated path of my local in settings.py

--- quizhub/settings.py
+++ quizhub/settings.py
@@ -58,7 +58,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://web-production-be9de.up.railway.app",
-    "https://localhost:5173",
+    "http://localhost:5173",
     "https://django-quiz-app-production.up.railway.app",
 ]
 
