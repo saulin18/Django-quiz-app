767edbfe232c09311fb3da7a134cb6cdf3711087
saulin18 <sondonsaul@gmail.com>
2024-10-07 03:06:15 -0400

    updated urls.py, path changes only

--- quizhub/quizes/urls.py
+++ quizhub/quizes/urls.py
@@ -3,7 +3,7 @@ from .views import quiz_list, quiz_create, quiz_winner_update, create_solution
 
 urlpatterns = [
     path('', quiz_list),
-    path('', quiz_create),
-    path('update/<int:pk>', quiz_winner_update),
-    path('solutions/<int:pk>', create_solution),
+    path('create/', quiz_create),
+    path('update/<int:pk>/', quiz_winner_update),
+    path('solutions/<int:pk>/', create_solution),
 ]
