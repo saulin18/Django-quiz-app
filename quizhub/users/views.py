75a4575dfe038f62ce53044ab00035d40ed9d97f
saulin18 <sondonsaul@gmail.com>
2024-10-06 21:01:09 -0400

    updated register action when a user is registering

--- quizhub/users/views.py
+++ quizhub/users/views.py
@@ -1,24 +1,33 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
+from rest_framework_simplejwt.tokens import RefreshToken
 from quizhub.users.models import RegisterUser
 from .serializers import MyTokenObtainPairSerializer, RegisterUserSerializer
 from .models import RegisterUser as CustomUser
+from rest_framework.permissions import AllowAny
+
 # Create your views here.
 
 @api_view(['POST'])
 def register(request):
-    username = request.POST.get('username')
-    password = request.POST.get('password')
-    user = CustomUser.objects.create_user(username=username, password=password)
-    serializer = RegisterUserSerializer(user, many=False)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  username = request.data.get('username')
+  password = request.data.get('password')
+  
+  if not username or not password:
+    return Response({'error': 'Username and password are required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+  user = CustomUser.objects.create_user(username=username, password=password)
+
+  refresh = RefreshToken.for_user(user)
+  token = {
+    'refresh': str(refresh),
+    'access': str(refresh.access_token),
+  }
+
+  return Response(token, status=status.HTTP_201_CREATED)
 
 @api_view(['GET'])
 def get_user(request):
